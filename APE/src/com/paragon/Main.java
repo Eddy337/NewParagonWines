44e610134bfae6e787fa03741f6ee94db88ca66e
Eddy337 <jshiner@lineone.net>
2013-08-26 15:05:03 +0100

    Finished APE project

--- APE/src/com/paragon/Main.java
+++ APE/src/com/paragon/Main.java
@@ -1,7 +1,7 @@
 package com.paragon;
 import com.paragon.orders.OrderLedger;
 import com.paragon.stock.Offer;
-import com.paragon.orders.Order;
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -11,13 +11,42 @@ public class Main {
         OrderService orderSystem = new OrderSystem();
         orderSystem.add(OrderLedger.getInstance());
         String userAuthToken = "tom@example.com";
-        List<Offer> searchResults = orderSystem.searchForProduct("Cabernet Sauvignon");
+        String searchString = "";
+        int timeDelay = 0;
+        DataInputStream dataInputStream = new DataInputStream(System.in);
+
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        System.out.println("Enter a product to search for:");
+
+        try {
+            searchString = bufferedReader.readLine();
+        }
+        catch (IOException ex) {
+            System.out.println("Exception detected in keyboard input");
+        }
+
+        System.out.println("Enter a delay in minutes:");
+
+        try {
+            timeDelay = Integer.parseInt(bufferedReader.readLine());
+        }
+        catch (NumberFormatException ex) {
+            System.out.println("Not a number!");
+        }
+
+        catch (IOException ex) {
+            System.out.println("Exception detected in keyboard input");
+        }
+
+        List<Offer> searchResults = orderSystem.searchForProduct(searchString);
         if (searchResults.isEmpty()) {
             System.out.println("No search results found");
         } else {
             Offer offer = searchResults.get(0);
             // some time may pass...
-            Thread.sleep(5 * 1000);
+            Thread.sleep(timeDelay * 1000);
             if (priceAcceptable(offer.price)) {
                 System.out.println(orderSystem.updateOrderLedger(orderSystem.confirmOrder(orderSystem.validQuote(offer.id), userAuthToken)));
             }
